ff1fdccc9c68727f2818ffc3f53293b0dd849578
Guilherme Aquino <31013254+itsmyvibee@users.noreply.github.com>
2025-08-21 23:01:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ if st.button("Calcular"):
         # Baixa o Excel do GitHub
         headers = {"Authorization": f"token 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"}
         
-        response = requests.get(EXCEL_URL)
+        response = requests.get(EXCEL_URL, headers=headers)
         response.raise_for_status()  # garante que a requisição deu certo
         file_stream = BytesIO(response.content)
 
